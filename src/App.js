e5cae9c74363d6b1efbec4ca37693d05e22a3e78
Lokesh Dhanasekar <lokdev@gmail.com>
2020-03-31 13:00:59 +0530

    Added addtional components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    My Hello World Lokesh
-    </div>
+class App extends Component {
+  render (){
+    return (
+      <div className="App">
+      My Hello World Lokesh
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
+      </div>
+    );
+  }
+}
+
+
+class FirstComponent extends Component{
+  render(){
+    return (
+      <div className ="FirstComponent">First Component</div>
+    );
+  }
+}
+
+class SecondComponent extends Component{
+  render(){
+    return(
+      <div className="SecondComponent">Second Component</div>
+    );
+  }
+}
+
+function ThirdComponent(){
+  return(
+    <div className="ThirdComponent"> Third Component</div>
   );
 }
 
+function FourthComponent(){
+  return(
+    <div className="FourthComponent">Fourth Component</div>
+  );
+}
 export default App;
